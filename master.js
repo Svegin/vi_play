8552ce0c427863e2ba4fc3e68172f1d0e95a90da
Svegin <svegin@Svegins-Mac-mini.local>
2017-01-04 20:36:57 -0600

    add change progress bar

--- master.js
+++ master.js
@@ -14,11 +14,29 @@ function togglePlay() {
   const method = video.paused ? 'play' : 'pause';
   video[method]();
 }
+// change buttons play/pause
 function updateButton() {
   const icon = this.paused ? '▶️' : '⏸';
-  console.log(icon);
   toggle.textContent = icon;
-  console.log('update the button');
+
+}
+// move time up/down
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+}
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+video.currentTime  = scrubTime;
 }
 
 
@@ -26,4 +44,15 @@ function updateButton() {
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+
 toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
